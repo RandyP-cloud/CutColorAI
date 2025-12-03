e0a8fdeec329f4c0f374c412bbcecb27129c3760
RandyP-cloud <debpetre@outlook.com>
2025-12-03 13:01:45 -0500

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>CutColor.AI - Pro Hair Transformation</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-      </head>
+      </Head>
       <CutColorAI />
     </>
   );
